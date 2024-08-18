8a1e7616763977fc675c27ee184507e0144d2e03
sunisha <sunishams2004@gmail.com>
2024-08-18 21:45:39 +0530

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-//checking palindrome or not
+{
+   //checking palindrome or not
 
-str = "malayalam"
- let split = str.split('');
-split.reverse();
+   str = "manam"
+   let split = str.split('');
+   split.reverse();
 
- let strjoin =split.join('');
- console.log(strjoin);
+   let strjoin = split.join('');
+   console.log(strjoin);
 
- if(strjoin == str){
-    console.log("it is a palindrome")
- }else{
- console.log("not a palindrome")
- }
+   if (strjoin == str) {
+      console.log("it is a palindrome")
+   } else {
+      console.log("not a palindrome")
+   }
+}
+
+
+{
+
+   //multiplication table
+   console.log("\n\n\n\n");
+   console.log("multiplication table")
+
+
+   function multiply(n) {
+      for (let i = 1; i <= 10; i++)
+         console.log(` ${i} x ${n} : `, n * i)
+   }
+
+
+   n = 6
+   console.log("number:",n);
+   multiply(n)
+}
